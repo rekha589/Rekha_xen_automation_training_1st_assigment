035b89bee6ed38e847735343fd388ba70e2773e6
Rekha Mane <rekha-neosoft@xendit.co>
2022-09-28 19:30:17 +0530

    Added left side tabs verification

--- cypress/e2e/2-advanced-examples/Demo.cy.js
+++ cypress/e2e/2-advanced-examples/Demo.cy.js
@@ -1,15 +1,66 @@
 
-describe('first step', () => {
-    it('I navigate to Xendit dashboard', () => {
+describe('Assignment 2nd', () => {
+    it('I login to Xendit dashboard', () => {
         cy.visit('https://xendit-dashboard-ui-live.us-west-2.stg.tidnex.dev/')
-    })
-    it('I input the correct username and password',() => {
         cy.get('input[name="email"]').type('rekha-neosoft@xendit.co')
         cy.get('input[name="password"]').type('Xendit@123')
-        cy.get('.d-flex > .btn').click()
+        cy.get('[type="submit"]').click()
     })
-    it('I should see device verification error message',()=>{
-        cy.wait(10000)
-        cy.get('.info-banner-text').contains('You are in Test Mode and any transactions made are simulated (not real)').should('be.visible')
+
+    it('Validate list of tabs shown on the left bar', () => {
+        cy.wait(30000)
+        let mainmenutabs = ["Home", "Balance", "Transactions", "Reporting", "Accept Payments", "Payment Links", "QR Codes", "Send Payments", "Customers"]
+        for (let index = 0; index < mainmenutabs.length; index++) {
+            switch (mainmenutabs[index]) {
+                case "Home":
+                    cy.get('#lhs-home').should('be.visible').and('have.text', 'Home')
+
+                    break;
+                case "Balance":
+                    cy.get('#lhs-balance').should('be.visible').and('have.text', 'Balance')
+
+                    break;
+                case "Transactions":
+                    cy.get('#lhs-transactions-new').should('be.visible').and('have.text', 'Transactions')
+
+                    break;
+                case "Reporting":
+                    cy.get('#lhs-reporting').should('be.visible').and('have.text', 'ReportingBeta')
+
+                    break;
+                case "Accept Payments":
+                    cy.get('#lhs-moneyin').should('be.visible').and('have.text', 'Accept Payments')
+
+                    break;
+                case "Payment Links":
+                    cy.get('#lhs-paymentlinks').should('be.visible').and('have.text', 'Payment Links')
+
+                    break;
+                case "QR codes":
+                    cy.get('#lhs-qr-code').should('be.visible').and('have.text', 'QR codes')
+
+                    break;
+                case "Send Payments":
+                    cy.get('#lhs-moneyout').should('be.visible').and('have.text', 'Send Payments')
+
+                    break;
+                case "Customers":
+                    cy.get('#lhs-customers').should('be.visible').and('have.text', 'Customers')
+
+                    break;
+
+
+            }
+
+        }
+
     })
+
+
+    // it('I should see device verification error message',()=>{
+    //     cy.wait(12000)
+    //     cy.get('.info-banner-content')
+    //       .contains('You are in Test Mode and any transactions made are simulated (not real)')
+    //       .should('be.visible')
+
 })
